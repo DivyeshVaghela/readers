8f0453f88a06d7dbf8f0d7a2237dcc79fe54e2d8
Divyesh Vaghela <divyeshv789@gmail.com>
2018-09-29 15:42:44 +0530

    Errors are resolved and platform is configured. Welcome to readers.

---
+++ readers/src/main/java/com/learning/readers/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.learning.readers.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value = {"/", "/index", "/home"})
+	public ModelAndView index() {
+		
+		ModelAndView mv = new ModelAndView("masterPage");
+		
+		mv.addObject("grettingMessage", "Welcome to Readers'");
+		
+		return mv;
+	}
+}
